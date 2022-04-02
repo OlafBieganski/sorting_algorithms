4c742aa5bd040f59f5c4e84812e64bc69ebdc216
Olaf Bieganski <olaf.bieganski2001@gmail.com>
2022-04-02 16:29:18 +0200

    main is ready

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "./sortingAlgorithms.hh"
 #include "arrayGenerator.hh"
 
@@ -8,14 +9,39 @@ using namespace std;
 
 int main(int argc, char ** argv){
 
-Array2D arr(100, 1000000);
-arr.fillArr2D(50, false);
-//arr.display();
-cout << "Array is sorted = " << arr.isSorted(true) << endl;
-cout << "Time of sorting = " << arr.sort(MERGE) << endl;
-//arr.display();
-cout << "Array is sorted = " << arr.isSorted(false) << endl;
+    float percents[] = {0, 25, 50, 75, 95, 99, 99.7};
+    int numOfElems[] = {10000, 50000, 100000, 500000, 1000000};
+    bool order[] = {0,0,0,0,0,0,0,1};
+    sortType sort[] = {MERGE, QUICK, INTROSPECTIVE};
+    string sortNames[] = {"Merge Sort", "Quick Sort", "Introspective Sort"};
+    int sizePerc = sizeof(percents) / sizeof(percents[0]);
+    int sizeElems = sizeof(numOfElems) / sizeof(numOfElems[0]);
 
-return 0;
+    fstream File;
+    string filename;
+    LOG("Podaj nazwe dla pliku z czasem wykonywania algorytmow (.txt):");
+    cin >> filename;
+    File.open(filename, ios::out);
+    if(!File.is_open()){
+        LOG("Blad otwarcia pliku.");
+        return 1;
+    }
+
+    for(int k = 0; k < 3; k++){
+        File << sortNames[k] << '\n';
+        for(int i = 0; i < sizePerc; i++){
+            for(int j = 0; j < sizeElems; j++){
+                Array2D arr(100, numOfElems[j]);
+                arr.fillArr2D(percents[i], order[i]);
+                //cout << "Array is sorted = " << arr.isSorted(order[i]) << endl;
+                File << arr.sort(sort[k]) << '\t';
+                cout << "Array is sorted = " << arr.isSorted(order[i]) << endl;
+            }
+            File << '\n';
+        }
+    }
+
+    File.close();
+    return 0;
 }
 
